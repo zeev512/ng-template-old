f7e09acd466eb4d35ee28a9ed7dfed4274c7bef0
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 06:03:49 +0300

    The console error we get on incerrect usage of multiple structural directives appied to the same ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,26 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
+/*
+This is an example of incorrect usag of multiple structureal directives.
+The console will through this error:
 
+Uncaught Error: Template parse errors:
+Can't have multiple template bindings on one element. Use only one attribute named 'template' or prefixed with * ("
+		<div class="lesson" *ngIf="lessons" [ERROR ->]*ngFor="let lesson of lessons">4
+			<div class="lesson-detail">
+				{{lesson | json}}
+"): ng:///AppModule/AppComponent.html@1:38
+    at syntaxError (compiler.es5.js:1690)
+    at TemplateParser.webpackJsonp.../../../compiler/@angular/compiler.es5.js.TemplateParser.parse (compiler.es5.js:12811)
+    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler._compileTemplate (compiler.es5.js:26963)
+    at compiler.es5.js:26883
+    at Set.forEach (<anonymous>)
+    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler._compileComponents (compiler.es5.js:26883)
+    at compiler.es5.js:26770
+    at Object.then (compiler.es5.js:1679)
+    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler._compileModuleAndComponents (compiler.es5.js:26769)
+    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler.compileModuleAsync (compiler.es5.js:26698)
+ */
 @Component({
 	selector: 'app-root',
 	template: `
