35a0bbf481390a182d88ee8c30075d110005fea7
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 11:14:24 +0300

    Clean up files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import 'rxjs/add/observable/of';
-import { TabContainerComponent } from './tab-container/tab-container.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TabContainerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
