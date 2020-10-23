5a8bdc3ecba9920201035c806e14c0efcd79a16d
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 05:52:30 +0300

    Revealing ng-template internally by Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,16 @@ import {Observable} from 'rxjs/Observable';
 @Component({
 	selector: 'app-root',
 	template: `
-		<ng-template>
-			<button class="tab-button" (click)="login()">{{loginText}}</button>
-			<button class="tab-button" (click)="signUp()">{{signUpText}}</button>
+		<ng-template [ngIf]="lessons" [ngIfElse]="loading">
+			<div class="lessons-list">
+				<button class="tab-button" (click)="login()">{{loginText}}</button>
+				<button class="tab-button" (click)="signUp()">{{signUpText}}</button>
+			</div>
 		</ng-template>
-	`
-})
+		<ng-template #loading>
+			<div>Loading...</div>
+		</ng-template>
+	`})
 export class AppComponent {
 
 	loginText = 'Login';
