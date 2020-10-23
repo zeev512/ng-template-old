dc7376d1aec055e2ed86008a8489fd6f99162bac
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 11:29:39 +0300

    Simple usage of Angular built-in *ngTemplateOutlet structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
 import 'rxjs/add/observable/of';
-import { TabContainerComponent } from './tab-container/tab-container.component';
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TabContainerComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
