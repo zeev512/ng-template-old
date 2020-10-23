a6ef24b284c47e1afd0d14a96ae2c2a54f0bd0d1
Zeev Lazarev <zlazarev@gmail.com>
2020-10-23 06:08:06 +0300

    Workaround for using multiple structural directive applied to the same ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,16 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 /*
-This is an example of incorrect usag of multiple structureal directives.
-The console will through this error:
-
-Uncaught Error: Template parse errors:
-Can't have multiple template bindings on one element. Use only one attribute named 'template' or prefixed with * ("
-		<div class="lesson" *ngIf="lessons" [ERROR ->]*ngFor="let lesson of lessons">4
-			<div class="lesson-detail">
-				{{lesson | json}}
-"): ng:///AppModule/AppComponent.html@1:38
-    at syntaxError (compiler.es5.js:1690)
-    at TemplateParser.webpackJsonp.../../../compiler/@angular/compiler.es5.js.TemplateParser.parse (compiler.es5.js:12811)
-    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler._compileTemplate (compiler.es5.js:26963)
-    at compiler.es5.js:26883
-    at Set.forEach (<anonymous>)
-    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler._compileComponents (compiler.es5.js:26883)
-    at compiler.es5.js:26770
-    at Object.then (compiler.es5.js:1679)
-    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler._compileModuleAndComponents (compiler.es5.js:26769)
-    at JitCompiler.webpackJsonp.../../../compiler/@angular/compiler.es5.js.JitCompiler.compileModuleAsync (compiler.es5.js:26698)
+This is workaround be which we add extra div element.
  */
 @Component({
 	selector: 'app-root',
 	template: `
-		<div class="lesson" *ngIf="lessons" *ngFor="let lesson of lessons">4
-			<div class="lesson-detail">
-				{{lesson | json}}
+		<div *ngIf="lessons">
+			<div class="lesson" *ngFor="let lesson of lessons">
+				<div class="lesson-detail">
+					{{lesson | json}}
+				</div>
 			</div>
 		</div>
 	`})
